82770b2bb4e8d6091972f490d6f430eb6bf8795e
Mickey Vänskä <mvaenskae@gmail.com>
2018-06-30 19:41:17 +0200

    ResNet18 ready to run after removing satImage_092 and it's heatmap.
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

--- train-mickey.py
+++ train-mickey.py
@@ -6,13 +6,13 @@ import datetime
 d = datetime.datetime.utcnow()  # <-- get time in UTC
 timestamp = d.isoformat("T") + "Z"
 
-EPOCH_COUNT = 3000
+EPOCH_COUNT = 100
 model_filename = 'model-weights_' + timestamp + '.h5'
 
 #model = InceptionResNet()
 #model = EasyNet()
-#model = ResNet(full_preactivation=False)
-model = RedNet(full_preactivation=False)
+model = ResNet(full_preactivation=False)
+#model = RedNet(full_preactivation=False)
 #model = SimpleNet()
 model.model.summary()
 model.train(EPOCH_COUNT)
